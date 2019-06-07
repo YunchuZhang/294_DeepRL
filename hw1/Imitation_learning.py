416590a8ec9ded128ed9adb93842403d87dff539
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2019-06-07 16:30:22 -0700

    Add Dagger

--- hw1/Imitation_learning.py
+++ hw1/Imitation_learning.py
@@ -1,7 +1,10 @@
 #Imitation learning
 """
 Example usage:
-	python imitation_learning.py expert_data/Humanoid-v2.pkl Humanoid-v2 --render --num_rollouts 200
+	python imitation_learning.py expert_data/Humanoid-v2.pkl Humanoid-v2 --render --num_rollouts 200 
+
+	Dagger
+	python imitation_learning.py expert_data/Humanoid-v2.pkl experts/Humanoid-v2.pkl Humanoid-v2 --render --num_rollouts 200 --Dagger_iter 10
 
 """
 
@@ -11,6 +14,7 @@ import numpy as np
 import gym
 import argparse
 import pickle
+import load_policy
 
 def build_model():
 
@@ -57,7 +61,7 @@ def main():
 	mse = tf.reduce_mean(0.5 * tf.square(output_pred - output_ph))
 
 	# create optimizer
-	opt = tf.train.AdamOptimizer(learning_rate=0.03).minimize(mse)
+	opt = tf.train.AdamOptimizer(learning_rate=0.00003).minimize(mse)
 
 	# initialize variables
 	sess.run(tf.global_variables_initializer())
@@ -79,7 +83,135 @@ def main():
 		if training_step % 200 == 0:
 			saver.save(sess, "store/model.ckpt")
 
+def Dagger():
+
+	tf.reset_default_graph()
+	sess = tf.Session()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('expert_policy_data', type=str)
+	parser.add_argument('expert_policy_file', type=str)
+	parser.add_argument('envname', type=str)
+	parser.add_argument('--render', action='store_true')
+	parser.add_argument("--max_timesteps", type=int)
+	parser.add_argument('--num_rollouts', type=int, default=20,
+						help='Number of expert roll outs')
+	parser.add_argument('--Dagger_iter', type = int, default=10)
+	args = parser.parse_args()
+
+	print('loading model')
+
+	# create the model
+	input_ph, output_ph, output_pred = build_model()
+
+	# restore the saved model
+	saver = tf.train.Saver()
+	saver.restore(sess, "store/model.ckpt")
+
+
+	print('loading and building expert policy')
+	policy_fn = load_policy.load_policy(args.expert_policy_file)
+
+	print('loaded and built')
+
+	with tf.Session():
+		#tf_util.initialize()
+
+		env = gym.make(args.envname)
+		max_steps = args.max_timesteps or env.spec.timestep_limit
+		returns = []
+		observations = []
+		actions = []
+		for i in range(args.Dagger_iter):
+			print('iter', i)
+			obs = env.reset()
+			done = False
+			totalr = 0.
+			steps = 0
+			while not done:
+				action = sess.run(output_pred, feed_dict={input_ph: obs[None,:]})
+				real_action = policy_fn(obs[None,:])
+				
+				#print(action)
+				observations.append(obs)
+				actions.append(real_action)
+				obs, r, done, _ = env.step(action)
+				totalr += r
+				steps += 1
+				if args.render:
+					env.render()
+				if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
+				if steps >= max_steps:
+					break
+			returns.append(totalr)
+
+		print('returns', returns)
+		print('mean return', np.mean(returns))
+		print('std of return', np.std(returns))
+
+
+		observations_n = np.array(observations)
+		actions_n = np.array(actions)
+
+
+
+	#Train
+	tf.reset_default_graph()
+	sess = tf.Session()
+	print('loading expert data')
+	filename = args.expert_policy_data
+
+	with open(filename, 'rb') as f:
+		data = pickle.loads(f.read())
+
+	# assert len(data.keys()) == 2
+	observations = data['observations']
+	actions = data['actions']
+
+	observations = np.squeeze(observations)
+	actions = np.squeeze(actions)
+	actions_n = np.squeeze(actions_n)
+
+	print(observations.shape)
+	print(observations_n.shape)
+
+	print(actions.shape)
+	print(actions_n.shape)
+
+	observations = np.concatenate((observations, observations_n), axis = 0)
+	#print(observations.shape)
+	actions = np.concatenate((actions, actions_n), axis = 0)
+	
+	input_ph, output_ph, output_pred = build_model()
+	
+	# create loss
+	mse = tf.reduce_mean(0.5 * tf.square(output_pred - output_ph))
+
+	# create optimizer
+	opt = tf.train.AdamOptimizer(learning_rate=0.00003).minimize(mse)
+
+	# initialize variables
+	sess.run(tf.global_variables_initializer())
+	# create saver to save model variables
+	saver = tf.train.Saver()
+
+	# run training
+	batch_size = 32
+	for training_step in range(10000):
+		# get a random subset of the training data
+		
+		# run the optimizer and get the mse
+		_,output_pred_run, mse_run = sess.run([opt,output_pred, mse], feed_dict={input_ph: observations, output_ph: actions})
+		if training_step % 100 == 0:
+			print('{0:04d} mse: {1:.3f}'.format(training_step, mse_run))
+			print((output_pred_run - actions).mean())
+			print((output_pred_run - actions).sum())
+		# print the mse every so often
+		if training_step % 200 == 0:
+			saver.save(sess, "store/model.ckpt")
+
+
 def test():
+
 	tf.reset_default_graph()
 	sess = tf.Session()
 	parser = argparse.ArgumentParser()
@@ -178,6 +310,7 @@ def gymtest():
 
 
 if __name__ == '__main__':
-	main()
-	test()
+	#main()
+	Dagger()
+	#test()
 	#gymtest()
